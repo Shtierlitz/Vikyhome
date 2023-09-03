e1059ab692e70049f75518786000c55ff9b63b90
Shtierlitz <rollbar1990@gmail.com>
2023-09-03 14:01:59 +0300

    Cacheing added

--- cleaning-service/api/cleaning/views.py
+++ cleaning-service/api/cleaning/views.py
@@ -5,27 +5,30 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from .models import Service, Extra
 from .serializers import ServiceSerializer, ExtraSerializer
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 
-class ServiceViewSet(viewsets.ModelViewSet):
+class CacheMixin:
+    @method_decorator(cache_page(60 * 15))
+    def dispatch(self, *args, **kwargs):
+        if self.request.method == 'GET':
+            return super().dispatch(*args, **kwargs)
+        return super().dispatch(*args, **kwargs)
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            self.permission_classes = [AllowAny, ]
+        else:
+            self.permission_classes = [IsAuthenticated, ]
+        return super().get_permissions()
+
+
+class ServiceViewSet(CacheMixin, viewsets.ModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
 
-    def get_permissions(self):
-        if self.request.method == 'GET':
-            self.permission_classes = [AllowAny, ]
-        else:
-            self.permission_classes = [IsAuthenticated, ]
-        return super(ServiceViewSet, self).get_permissions()
 
-
-class ExtraViewSet(viewsets.ModelViewSet):
+class ExtraViewSet(CacheMixin, viewsets.ModelViewSet):
     queryset = Extra.objects.all()
     serializer_class = ExtraSerializer
-
-    def get_permissions(self):
-        if self.request.method == 'GET':
-            self.permission_classes = [AllowAny, ]
-        else:
-            self.permission_classes = [IsAuthenticated, ]
-        return super(ExtraViewSet, self).get_permissions()
